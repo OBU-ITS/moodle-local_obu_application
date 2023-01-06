676952fbadc7953720ea9404faf4c42c551cc233
EmirK224 <p0092396@brookes.ac.uk>
2023-01-06 15:56:01 +0000

    edited first line on registration page slightly and added requested message for the incident

--- lang/en/local_obu_application.php
+++ lang/en/local_obu_application.php
@@ -117,7 +117,7 @@ $string['privacy'] = 'Privacy';
 $string['privacy_text'] = 'Privacy statement';
 
 $string['registration'] = 'Registration';
-$string['registrationsteps'] = 'To access to this site you will firstly need to take a minute to register yourself. Here are the steps: <ol> <li>Fill out the <a href="{$a}">Registration</a> form with your details.</li> <li>An email will be immediately sent to your email address.</li> <li>Read your email, and click on the web link it contains.</li> <li>Your account will be confirmed and you will be logged in.</li> <li>You can now access the site. From now on you will only need to enter your personal email and password to log in to your account.</li> </ol>';
+$string['registrationsteps'] = 'To access this site you will firstly need to take a minute to register yourself. Here are the steps: <ol> <li>Fill out the <a href="{$a}">Registration</a> form with your details.</li> <li>An email will be immediately sent to your email address.</li> <li>Read your email, and click on the web link it contains.</li> <li>Your account will be confirmed and you will be logged in.</li> <li>You can now access the site. From now on you will only need to enter your personal email and password to log in to your account.</li> </ol> Please note, do not use an NHS email address for registering. The NHS firewall may filter out our replies, which will mean you cannot access the portal to apply. Once you have registered, you can enter your NHS email address in the Contact Details section, and this will become your primary contact email until you have completed enrolment.';
 
 $string['page_content'] = '<h2>How to Apply</h2>
 Using the links above, please:
