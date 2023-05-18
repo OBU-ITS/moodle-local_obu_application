df6d4dd7afcd96baebafd757d2106114e3eab5d1
EmirK224 <p0092396@brookes.ac.uk>
2023-05-18 12:20:37 +0100

    updating version and release in version file

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  */
 
 $plugin->component = 'local_obu_application'; // Full name of the plugin (used for diagnostics): plugintype_pluginname
-$plugin->version  = 2023010901;   // The (date) version of this module + 2 extra digital for daily versions
+$plugin->version  = 2023051801;   // The (date) version of this module + 2 extra digital for daily versions
 $plugin->requires = 2015111604;   // Requires this Moodle version - at least 3.0.4
 $plugin->cron     = 0;
-$plugin->release = 'v2.5.8';
+$plugin->release = 'v2.6.0';
 $plugin->maturity = MATURITY_STABLE;
