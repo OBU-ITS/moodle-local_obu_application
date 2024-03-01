567f5fa71b6fc063102399ace41e22a7eaa5e432
Emir Kamel <p0092396@brookes.ac.uk>
2024-03-01 14:33:17 +0000

    removing required and validation for personal email and adding preamble

--- profile_contact_details_form.php
+++ profile_contact_details_form.php
@@ -128,18 +128,18 @@ class profile_contact_details_form extends moodleform {
             $mform->addElement('text', 'email', get_string('email'), 'size="25" maxlength="100"');
             $mform->disabledIf('email', 'firstname', 'neq', '?****?');
 
+            $mform->addElement('html', '<p><strong>' . get_string('personalemail_preamble', 'local_obu_application') . '</strong></p>');
             $mform->addElement('text', 'personalemail', get_string('personalemail', 'local_obu_applications'), 'size="25" maxlength="100"');
             $mform->setType('personalemail', PARAM_RAW_TRIMMED);
-            $mform->addRule('personalemail', get_string('missingemail'), 'required', null, 'server');
         } else {
             $mform->addElement('header', 'newemail', get_string('newemail', 'local_obu_application'), '');
             $mform->addElement('text', 'email', get_string('email'), 'size="25" maxlength="100"');
             $mform->setType('email', PARAM_RAW_TRIMMED);
             $mform->addRule('email', get_string('missingemail'), 'required', null, 'server');
 
+            $mform->addElement('html', '<p><strong>' . get_string('personalemail_preamble', 'local_obu_application') . '</strong></p>');
             $mform->addElement('text', 'personal_email', get_string('personalemail', 'local_obu_applications'), 'size="25" maxlength="100"');
             $mform->setType('personal_email', PARAM_RAW_TRIMMED);
-            $mform->addRule('personal_email', get_string('missingemail'), 'required', null, 'server');
 
             $mform->addElement('text', 'username', get_string('confirm_email', 'local_obu_application'), 'size="25" maxlength="100"');
             $mform->setType('username', PARAM_RAW_TRIMMED);
@@ -171,9 +171,6 @@ class profile_contact_details_form extends moodleform {
             } else if ($data['username'] != $data['email']) {
                 $errors['username'] = get_string('invalidemail');
             }
-        }
-        if (!validate_email($data['personal_email']) || ($data['personal_email'] != strtolower($data['personal_email']))) {
-            $errors['personal_email'] = get_string('invalidemail');
         }
 
         if (!empty($errors)) {
