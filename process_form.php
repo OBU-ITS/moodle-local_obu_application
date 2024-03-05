2f25cf31d6ae6a26cafe39098d568f5e14fc9c38
Emir Kamel <p0092396@brookes.ac.uk>
2024-03-05 17:13:24 +0000

    changing Approval head on applications to be h3 instead of h1, adding label to comment field

--- process_form.php
+++ process_form.php
@@ -514,19 +514,19 @@ class process_form extends moodleform {
 				$mform->addElement('static', 'approval', '');
 				$mform->closeHeaderBefore('approval');
                 if ($approval_sought == 1){
-                    $mform->addElement('html', '<h1>' . get_string('approval_head', 'local_obu_application') . '</h1>');
+                    $mform->addElement('html', '<h3>' . get_string('approval_head', 'local_obu_application') . '</h3>');
                     $mform->addElement('html', '<p><strong>' . get_string('manager_comment', 'local_obu_application') . '</strong></p>');
-                    $mform->addElement('text', 'comment', '', 'size="40" maxlength="100"');
+                    $mform->addElement('text', 'comment', 'Comment:', 'size="40" maxlength="100"');
                     $mform->setType('comment', PARAM_TEXT);
                 } else if ($approval_sought == 2){
-                    $mform->addElement('html', '<h1>' . get_string('approval_head', 'local_obu_application') . '</h1>');
+                    $mform->addElement('html', '<h3>' . get_string('approval_head', 'local_obu_application') . '</h3>');
                     $mform->addElement('html', '<p><strong>' . get_string('funder_comment', 'local_obu_application') . '</strong></p>');
-                    $mform->addElement('text', 'comment', '', 'size="40" maxlength="100"');
+                    $mform->addElement('text', 'comment', 'Comment:', 'size="40" maxlength="100"');
                     $mform->setType('comment', PARAM_TEXT);
                 } else {
-                    $mform->addElement('html', '<h1>' . get_string('approval_head', 'local_obu_application') . '</h1>');
+                    $mform->addElement('html', '<h3>' . get_string('approval_head', 'local_obu_application') . '</h3>');
                     $mform->addElement('html', '<p><strong>' . get_string('admin_comment', 'local_obu_application') . '</strong></p>');
-                    $mform->addElement('text', 'comment', '', 'size="40" maxlength="100"');
+                    $mform->addElement('text', 'comment', 'Comment:', 'size="40" maxlength="100"');
                     $mform->setType('comment', PARAM_TEXT);
                 }
 				$buttonarray[] = &$mform->createElement('submit', 'rejectbutton', get_string('reject', 'local_obu_application'));
