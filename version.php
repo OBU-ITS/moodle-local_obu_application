ed6b594e2e8c184b85e2dcacc246a58279c40d02
Emir Kamel <p0092396@brookes.ac.uk>
2024-05-20 12:00:41 +0100

    adding new sql to upgrade.php to remove old/unused courses. incrementing version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
  */
 
 $plugin->component = 'local_obu_application'; // Full name of the plugin (used for diagnostics): plugintype_pluginname
-$plugin->version  = 2024051702;   // The (date) version of this module + 2 extra digital for daily versions
+$plugin->version  = 2024052001;   // The (date) version of this module + 2 extra digital for daily versions
 $plugin->requires = 2015111604;   // Requires this Moodle version - at least 3.0.4
 $plugin->cron     = 0;
 $plugin->release = 'v3.1.3.2';
