54ff8754a8caa0422e3636ceaba9b724625a1486
Emir Kamel <p0092396@brookes.ac.uk>
2024-08-07 12:07:23 +0100

    da bug ing

--- mdl_xfer.php
+++ mdl_xfer.php
@@ -100,26 +100,29 @@ else if ($mform_data = $mform->get_data()) {
 	}
 
     $file_id = $xfer_id != 0 ? $xfer_id : $batch_number;
-
-	$applications = local_obu_application_get_applications(); // Get all applications
-	$xfers = array();
-	foreach ($applications as $application) {
-		if ((($application->approval_level == 3) && ($application->approval_state == 2)) // Approved by HLS so is/was OK to go...
-				&& (((($mform_data->xfer_type == 1) || ($mform_data->xfer_type == 3))
-					&& ($application->studying <> 2) && ($application->admissions_xfer == $xfer_id)) // Admissions or Process (Admissions data processing)
-				|| (($mform_data->xfer_type == 2) && ($application->finance_xfer == $xfer_id)))) { // Finance
-			// OK - check the date if necessary
-			if (($start_date == 0) || !isset($months[substr($application->course_date, 0, 3)])) { // No check (or we can't)
-				$course_date = 0;
-			} else {
-				$course_date = (($current_century_prefix . substr($application->course_date, 3)) * 100) + $months[substr($application->course_date, 0, 3)];
-			}
-			if ($course_date <= $start_date) {
-				$xfers[] = $application->id;
-			}
-		}
-	}
-
+    try {
+        $applications = local_obu_application_get_applications(); // Get all applications
+        $xfers = array();
+        foreach ($applications as $application) {
+            if ((($application->approval_level == 3) && ($application->approval_state == 2)) // Approved by HLS so is/was OK to go...
+                && (((($mform_data->xfer_type == 1) || ($mform_data->xfer_type == 3))
+                        && ($application->studying <> 2) && ($application->admissions_xfer == $xfer_id)) // Admissions or Process (Admissions data processing)
+                    || (($mform_data->xfer_type == 2) && ($application->finance_xfer == $xfer_id)))) { // Finance
+                // OK - check the date if necessary
+                if (($start_date == 0) || !isset($months[substr($application->course_date, 0, 3)])) { // No check (or we can't)
+                    $course_date = 0;
+                } else {
+                    $course_date = (($current_century_prefix . substr($application->course_date, 3)) * 100) + $months[substr($application->course_date, 0, 3)];
+                }
+                if ($course_date <= $start_date) {
+                    $xfers[] = $application->id;
+                }
+            }
+        }
+    } catch (Exception $e){
+        var_dump($e);
+        exit();
+    }
 
 	if (empty($xfers)) {
 		$message = get_string('no_xfer', 'local_obu_application');
