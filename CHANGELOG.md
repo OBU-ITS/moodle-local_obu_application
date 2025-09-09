decdee3e62d624b0a8c2b59d2dfe2b73bd973956
Emir Kamel <p0092396@brookes.ac.uk>
2025-09-09 18:03:02 +0100

    release number change, adding changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Student Experience Team | OBU Application ChangeLog
+
+> Keep entries newest → oldest. Use 4-part versions (e.g., 2.1.4.2).  
+> Release types: Major Release | Minor Release | Revision | Hotfix
+
+---
+
+## v3.3.0.0 – Minor Change
+**Date:** 2025-09-09  
+**Highlights:**
+- Removing use of deprecated <b>print_error</b> function, replacing with Moodle exception.
+- Introduction of changelog file
--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component = 'local_obu_application'; // Full name of the plugin (used f
 $plugin->version  = 2025090901;   // The (date) version of this module + 2 extra digital for daily versions
 $plugin->requires = 2015111604;   // Requires this Moodle version - at least 3.0.4
 $plugin->cron     = 0;
-$plugin->release = 'v3.2.2.0';
+$plugin->release = 'v3.3.0.0';
 $plugin->maturity = MATURITY_STABLE;
